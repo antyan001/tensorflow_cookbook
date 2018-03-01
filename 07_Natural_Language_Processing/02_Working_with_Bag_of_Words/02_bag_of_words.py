59af8798fba03c9c9e1e95cc2a57d28b71c33f79
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-03-01 16:20:35 +0100

    bag of words model update

--- 07_Natural_Language_Processing/02_Working_with_Bag_of_Words/02_bag_of_words.py
+++ 07_Natural_Language_Processing/02_Working_with_Bag_of_Words/02_bag_of_words.py
@@ -40,13 +40,8 @@ else:
   r = requests.get(zip_url)
   z = ZipFile(io.BytesIO(r.content))
   file = z.read('SMSSpamCollection')
-  # Format Data
-  # text_data = file.decode()
-  # text_data = text_data.encode('ascii', errors='ignore')
   text_data = file.split('\n')
   text_data = [x.split('\t') for x in text_data if len(x) >= 1]
-
-  # And write to csv
   with open(save_file_name, 'w') as temp_output_file:
     writer = csv.writer(temp_output_file)
     writer.writerows(text_data)
@@ -58,16 +53,9 @@ target = [x[0] for x in text_data]
 target = [1 if x == 'spam' else 0 for x in target]
 
 # Normalize text
-# Lower case
 texts = [x.lower() for x in texts]
-
-# Remove punctuation
 texts = [''.join(c for c in x if c not in string.punctuation) for x in texts]
-
-# Remove numbers
 texts = [''.join(c for c in x if c not in '0123456789') for x in texts]
-
-# Trim extra whitespace
 texts = [' '.join(x.split()) for x in texts]
 
 # Plot histogram of text lengths
@@ -75,6 +63,7 @@ text_lengths = [len(x.split()) for x in texts]
 text_lengths = [x for x in text_lengths if x < 50]
 plt.hist(text_lengths, bins=25)
 plt.title('Histogram of # of Words in Texts')
+plt.show()
 
 # Choose max text word length at 25
 sentence_size = 25
@@ -86,6 +75,7 @@ vocab_processor = learn.preprocessing.VocabularyProcessor(sentence_size, min_fre
 # Have to fit transform to get length of unique words.
 transform = vocab_processor.transform(texts)
 embedding_size = len([x for x in transform])
+print('embedding_size=%d' % embedding_size)
 
 # Split up data set into train/test
 train_indices = np.random.choice(len(texts), int(len(texts) * 0.8), replace=False)
@@ -96,35 +86,30 @@ target_train = [x for ix, x in enumerate(target) if ix in train_indices]
 target_test = [x for ix, x in enumerate(target) if ix in test_indices]
 
 # Model
+# Bag-of-words: feature vector consists of the number of times each token is present:
+# [10, 0, 0, 0, 0, 2, 0, 0, 0, 1, ... ] -> the words are 0, 5 and 9, in any order.
 
-# Setup Index Matrix for one-hot-encoding
-identity_mat = tf.diag(tf.ones(shape=[embedding_size]))
+# hard-coded batch_size=1
+x = tf.placeholder(shape=[sentence_size], dtype=tf.int32)
+y = tf.placeholder(shape=[1, 1], dtype=tf.float32)
 
-# Create variables for logistic regression
-A = tf.Variable(tf.random_normal(shape=[embedding_size, 1]))
+# Text-vocab embedding (id matrix for one-hot-encoding)
+identity_matrix = tf.diag(tf.ones(shape=[embedding_size]))
+x_embed = tf.nn.embedding_lookup(identity_matrix, x)
+x_embed = tf.Print(x_embed, data=[x_embed], message='Embedding', summarize=10, first_n=0)
+x_col_sums = tf.reduce_sum(x_embed, axis=0)
+x_col_sums = tf.Print(x_col_sums, data=[x_col_sums], message='x-sum', summarize=10, first_n=0)
+
+# Logistic regression model (one hidden layer)
+W = tf.Variable(tf.random_normal(shape=[embedding_size, 1]))
 b = tf.Variable(tf.random_normal(shape=[1, 1]))
-
-# Initialize placeholders
-x_data = tf.placeholder(shape=[sentence_size], dtype=tf.int32)
-y_target = tf.placeholder(shape=[1, 1], dtype=tf.float32)
-
-# Text-Vocab Embedding
-x_embed = tf.nn.embedding_lookup(identity_mat, x_data)
-x_col_sums = tf.reduce_sum(x_embed, 0)
-
-# Declare model operations
 x_col_sums_2D = tf.expand_dims(x_col_sums, 0)
-model_output = tf.add(tf.matmul(x_col_sums_2D, A), b)
+model_output = tf.add(tf.matmul(x_col_sums_2D, W), b)
 
 # Declare loss function (Cross Entropy loss)
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=model_output, labels=y_target))
-
-# Prediction operation
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=model_output, labels=y))
 prediction = tf.sigmoid(model_output)
-
-# Declare optimizer
-my_opt = tf.train.GradientDescentOptimizer(0.001)
-train_step = my_opt.minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
@@ -137,8 +122,7 @@ with tf.Session() as sess:
   for ix, t in enumerate(vocab_processor.fit_transform(texts_train)):
     y_data = [[target_train[ix]]]
 
-    sess.run(train_step, feed_dict={x_data: t, y_target: y_data})
-    temp_loss = sess.run(loss, feed_dict={x_data: t, y_target: y_data})
+    _, temp_loss = sess.run([train_step, loss], feed_dict={x: t, y: y_data})
     loss_vec.append(temp_loss)
 
     if (ix + 1) % 10 == 0:
@@ -146,8 +130,8 @@ with tf.Session() as sess:
 
     # Keep trailing average of past 50 observations accuracy
     # Get prediction of single observation
-    [[temp_pred]] = sess.run(prediction, feed_dict={x_data: t, y_target: y_data})
-    # Get True/False if prediction is accurate
+    temp_pred = sess.run(prediction, feed_dict={x: t, y: y_data})
+
     train_acc_temp = target_train[ix] == np.round(temp_pred)
     train_acc_all.append(train_acc_temp)
     if len(train_acc_all) >= 50:
@@ -164,7 +148,7 @@ with tf.Session() as sess:
 
       # Keep trailing average of past 50 observations accuracy
     # Get prediction of single observation
-    [[temp_pred]] = sess.run(prediction, feed_dict={x_data: t, y_target: y_data})
+    [[temp_pred]] = sess.run(prediction, feed_dict={x: t, y: y_data})
     # Get True/False if prediction is accurate
     test_acc_temp = target_test[ix] == np.round(temp_pred)
     test_acc_all.append(test_acc_temp)
