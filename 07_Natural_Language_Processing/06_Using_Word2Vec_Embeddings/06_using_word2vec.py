732a6aa8179b3e66f34a527e9733b92343ce06eb
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-03-02 21:22:15 +0100

    07.06: load CBOW to classify movies

--- 07_Natural_Language_Processing/06_Using_Word2Vec_Embeddings/06_using_word2vec.py
+++ 07_Natural_Language_Processing/06_Using_Word2Vec_Embeddings/06_using_word2vec.py
@@ -1,32 +1,30 @@
 # Using Word2Vec for prediction
-#---------------------------------------
+# ---------------------------------------
 #
 # In this example, we will load our prior CBOW trained embeddings
 #   to perform logistic regression model for movie review predictions
 #
 # From this data set we will compute/fit the CBOW model of
 #  the Word2Vec Algorithm
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-import random
 import os
 import pickle
-import string
-import requests
-import collections
-import io
-import tarfile
-import urllib.request
+
+import matplotlib.pyplot as plt
+import nltk
+import numpy as np
+import tensorflow as tf
 import text_helpers
 from nltk.corpus import stopwords
-from tensorflow.python.framework import ops
-ops.reset_default_graph()
+
+nltk.data.path.append('/home/maxim/bin/nltk')
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-# Start a graph session
-sess = tf.Session()
+
+########################################################################################################################
+# Data
+########################################################################################################################
+
 
 # Declare model parameters
 embedding_size = 200
@@ -34,24 +32,16 @@ vocabulary_size = 2000
 batch_size = 100
 max_words = 100
 
-# Declare stop words
-stops = stopwords.words('english')
-
-# Load Data
-print('Loading Data')
-data_folder_name = 'temp'
 texts, target = text_helpers.load_movie_data()
 
 # Normalize text
-print('Normalizing Text Data')
+stops = stopwords.words('english')
 texts = text_helpers.normalize_text(texts, stops)
-
-# Texts must contain at least 3 words
 target = [target[ix] for ix, x in enumerate(texts) if len(x.split()) > 2]
 texts = [x for x in texts if len(x.split()) > 2]
 
 # Split up data set into train/test
-train_indices = np.random.choice(len(target), round(0.8*len(target)), replace=False)
+train_indices = np.random.choice(len(target), round(0.8 * len(target)), replace=False)
 test_indices = np.array(list(set(range(len(target))) - set(train_indices)))
 texts_train = [x for ix, x in enumerate(texts) if ix in train_indices]
 texts_test = [x for ix, x in enumerate(texts) if ix in test_indices]
@@ -59,7 +49,7 @@ target_train = np.array([x for ix, x in enumerate(target) if ix in train_indices
 target_test = np.array([x for ix, x in enumerate(target) if ix in test_indices])
 
 # Load dictionary and embedding matrix
-dict_file = os.path.join( '..', '05_Working_With_CBOW_Embeddings', 'temp', 'movie_vocab.pkl')
+dict_file = os.path.join('..', '05_Working_With_CBOW_Embeddings', 'temp', 'movie_vocab.pkl')
 word_dictionary = pickle.load(open(dict_file, 'rb'))
 
 # Convert texts to lists of indices
@@ -67,85 +57,72 @@ text_data_train = np.array(text_helpers.text_to_numbers(texts_train, word_dictio
 text_data_test = np.array(text_helpers.text_to_numbers(texts_test, word_dictionary))
 
 # Pad/crop movie reviews to specific length
-text_data_train = np.array([x[0:max_words] for x in [y+[0]*max_words for y in text_data_train]])
-text_data_test = np.array([x[0:max_words] for x in [y+[0]*max_words for y in text_data_test]])
+text_data_train = np.array([x[0:max_words] for x in [y + [0] * max_words for y in text_data_train]])
+text_data_test = np.array([x[0:max_words] for x in [y + [0] * max_words for y in text_data_test]])
 
-print('Creating Model')
-# Define Embeddings:
-embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
 
-# Define model:
-# Create variables for logistic regression
-A = tf.Variable(tf.random_normal(shape=[embedding_size,1]))
-b = tf.Variable(tf.random_normal(shape=[1,1]))
+########################################################################################################################
+# Model
+########################################################################################################################
 
-# Initialize placeholders
 x_data = tf.placeholder(shape=[None, max_words], dtype=tf.int32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
-# Lookup embeddings vectors
+embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
 embed = tf.nn.embedding_lookup(embeddings, x_data)
-# Take average of all word embeddings in documents
-embed_avg = tf.reduce_mean(embed, 1)
+embed_avg = tf.reduce_mean(embed, axis=1)
 
-# Declare logistic model (sigmoid in loss function)
-model_output = tf.add(tf.matmul(embed_avg, A), b)
+W = tf.Variable(tf.random_normal(shape=[embedding_size, 1]))
+b = tf.Variable(tf.random_normal(shape=[1, 1]))
+model_output = tf.add(tf.matmul(embed_avg, W), b)
 
-# Declare loss function (Cross Entropy loss)
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=model_output, labels=y_target))
-
-# Actual Prediction
 prediction = tf.round(tf.sigmoid(model_output))
 predictions_correct = tf.cast(tf.equal(prediction, y_target), tf.float32)
 accuracy = tf.reduce_mean(predictions_correct)
 
-# Declare optimizer
-my_opt = tf.train.AdagradOptimizer(0.005)
-train_step = my_opt.minimize(loss)
+train_step = tf.train.AdagradOptimizer(0.005).minimize(loss)
 
-# Intitialize Variables
-init = tf.global_variables_initializer()
-sess.run(init)
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
 
-# Load model embeddings
-model_checkpoint_path = os.path.join( '..', '05_Working_With_CBOW_Embeddings',
-                                     'temp','cbow_movie_embeddings.ckpt')
-saver = tf.train.Saver({"embeddings": embeddings})
-saver.restore(sess, model_checkpoint_path)
+  # Load model embeddings
+  model_checkpoint_path = os.path.join('..', '05_Working_With_CBOW_Embeddings', 'temp', 'cbow_movie_embeddings.ckpt')
+  saver = tf.train.Saver({"embeddings": embeddings})
+  saver.restore(sess, model_checkpoint_path)
 
+  # Start Logistic Regression
+  print('Starting Model Training')
+  train_loss = []
+  test_loss = []
+  train_acc = []
+  test_acc = []
+  i_data = []
 
-# Start Logistic Regression
-print('Starting Model Training')
-train_loss = []
-test_loss = []
-train_acc = []
-test_acc = []
-i_data = []
-for i in range(10000):
+  for i in range(10000):
     rand_index = np.random.choice(text_data_train.shape[0], size=batch_size)
     rand_x = text_data_train[rand_index]
     rand_y = np.transpose([target_train[rand_index]])
     sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
-    
-    # Only record loss and accuracy every 100 generations
-    if (i+1)%100==0:
-        i_data.append(i+1)
-        train_loss_temp = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
-        train_loss.append(train_loss_temp)
-        
-        test_loss_temp = sess.run(loss, feed_dict={x_data: text_data_test, y_target: np.transpose([target_test])})
-        test_loss.append(test_loss_temp)
-        
-        train_acc_temp = sess.run(accuracy, feed_dict={x_data: rand_x, y_target: rand_y})
-        train_acc.append(train_acc_temp)
-    
-        test_acc_temp = sess.run(accuracy, feed_dict={x_data: text_data_test, y_target: np.transpose([target_test])})
-        test_acc.append(test_acc_temp)
-    if (i+1)%500==0:
-        acc_and_loss = [i+1, train_loss_temp, test_loss_temp, train_acc_temp, test_acc_temp]
-        acc_and_loss = [np.round(x,2) for x in acc_and_loss]
-        print('Generation # {}. Train Loss (Test Loss): {:.2f} ({:.2f}). Train Acc (Test Acc): {:.2f} ({:.2f})'.format(*acc_and_loss))
 
+    # Only record loss and accuracy every 100 generations
+    if (i + 1) % 100 == 0:
+      i_data.append(i + 1)
+      train_loss_temp = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+      train_loss.append(train_loss_temp)
+
+      test_loss_temp = sess.run(loss, feed_dict={x_data: text_data_test, y_target: np.transpose([target_test])})
+      test_loss.append(test_loss_temp)
+
+      train_acc_temp = sess.run(accuracy, feed_dict={x_data: rand_x, y_target: rand_y})
+      train_acc.append(train_acc_temp)
+
+      test_acc_temp = sess.run(accuracy, feed_dict={x_data: text_data_test, y_target: np.transpose([target_test])})
+      test_acc.append(test_acc_temp)
+    if (i + 1) % 500 == 0:
+      acc_and_loss = [i + 1, train_loss_temp, test_loss_temp, train_acc_temp, test_acc_temp]
+      acc_and_loss = [np.round(x, 2) for x in acc_and_loss]
+      print('Generation # {}. Train Loss (Test Loss): {:.2f} ({:.2f}). Train Acc (Test Acc): {:.2f} ({:.2f})'.format(*acc_and_loss))
 
 # Plot loss over time
 plt.plot(i_data, train_loss, 'k-', label='Train Loss')
